9e936efe33a802c6a459ff28526b1b09b96165ee
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-01 22:34:06 -0500

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   def self.from_omniauth(data={})
-    User.where(provider: data[:provider], uid: data[:uid]).first_or_create do |user|
+    User.where(provider: data[:info][:email]).first_or_create do |user|
       user.password = data[:info][:password]
       user.email = data[:info][:email]
       user.name = data[:info][:name]
