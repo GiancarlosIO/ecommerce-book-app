b7bacabfac188b19961e855ea984c4640a68af40
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-01 22:32:13 -0500

    Add header and footer components to routes

--- app/javascript/app/routes/index.js
+++ app/javascript/app/routes/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// Components
+// =========== Components ===========
 import App from '../components/app';
 import NotFound from '../components/public-pages/not-found';
+// Layout
+import Header from '../components/navigation/header';
+import Footer from '../components/navigation/footer';
+// authentication
+import Register from '../components/auth/register';
+import SignIn from '../components/auth/sign-in';
+import Profile from '../components/auth/profile';
 
 
 export default (
   <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/register" component={Register} />
+        <Route path="/sign_in" component={SignIn} />
+        <Route path="/profile" component={Profile} />
+        <Route component={NotFound} />
+      </Switch>
+      <footer />
+    </div>
   </Router>
 )
