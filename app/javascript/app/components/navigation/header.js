fce2125baff9cbc659deaeb766d7759ecfd54fb3
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-03 13:16:35 -0500

    Hide links when user is logged

--- app/javascript/app/components/navigation/header.js
+++ app/javascript/app/components/navigation/header.js
@@ -3,9 +3,32 @@ import { connect } from 'react-redux';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import CustomLink from './custom-link';
+import { signoutUser } from '../../actions/auth-actions';
 
 export class Header extends Component {
+
+  handleClickSignout = () => {
+    this.props.dispatch(signoutUser());
+  }
+
+  renderLinks = () => {
+    const { authenticated } = this.props;
+    if (authenticated) {
+      return [
+        <CustomLink key={1} to="/profile" label="Profile"/>,
+        <CustomLink key={2} to="/products" label="Products" />
+      ]
+    } else {
+      return [
+        <CustomLink key={1} to="/register" label="Register" />,
+        <CustomLink key={2} to="/sign_in" label="Sign in"/>,
+        <CustomLink key={3} to="/products" label="Products" />
+      ]
+    }
+  }
+
   render() {
+    const { authenticated } = this.props;
     return (
       <Navbar inverse collapseOnSelect>
         <Navbar.Header>
@@ -16,20 +39,23 @@ export class Header extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <CustomLink to="/register" label="Register" />
-            <CustomLink to="/sign_in" label="Sign in"/>
-            <CustomLink to="/profile" label="Profile"/>
+            {this.renderLinks()}
           </Nav>
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a>Sign out</a>
-            </li>
-          </ul>
+            {
+              authenticated &&
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <a onClick={this.handleClickSignout} >Sign out</a>
+                </li>
+              </ul>
+            }
         </Navbar.Collapse>
       </Navbar>
     )
   }
 };
 
-const HeaderConnected = connect()(Header);
+const mapStateToProps = (state) => ({ authenticated: state.auth.authenticated });
+
+const HeaderConnected = connect(mapStateToProps)(Header);
 export default withRouter(HeaderConnected);
