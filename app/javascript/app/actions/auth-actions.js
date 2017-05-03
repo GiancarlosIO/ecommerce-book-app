531c91e0212dc082ab2e9e400f2137862ceb5010
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-03 13:16:18 -0500

    Refactor auth-actions

--- app/javascript/app/actions/auth-actions.js
+++ app/javascript/app/actions/auth-actions.js
@@ -5,6 +5,12 @@ import {
   AUTH_ERROR
 } from '../constants/';
 
+import {
+  resetSession,
+  setSession,
+  getSession
+} from '../utils/api/header-config';
+
 export const authUser = () => ({ type: AUTH_USER });
 export const setUserData = (user) => ({ type: SET_USER_DATA, payload: user });
 export const authError = (error) => ({ type: AUTH_ERROR, payload: error });
@@ -12,18 +18,50 @@ export const unauthUser = () => ({ type: UNAUTH_USER });
 
 // Async actions
 
-export const signupUser = ({email, password, password_confirmation}) => {
-  return (dispatch, getState) => {
-
+export const signupUser = (email, password) => {
+  return (dispatch, getState, { AuthAPI }) => {
+    return AuthAPI.signup(email, password).request
+      .then(response => {
+        console.log('signup user successfully', response);
+        dispatch(authUser());
+        dispatch(setUserData(response.data.user));
+        setSession(response.data.user, response.data.session);
+      })
+      .catch( error => {
+        console.log('error to signup user', error.response);
+        dispatch(authError(error.response.data.error.user));
+      });
   }
 };
 
-export const signinUser = (password, email) => {
-  return (dispatch, getState) => {
-
+export const signinUser = (email, password) => {
+  return (dispatch, getState, { AuthAPI }) => {
+    return AuthAPI.signin(email, password).request
+      .then(response => {
+        console.log('signin user successfully', response);
+        dispatch(authUser());
+        dispatch(setUserData(response.data.user));
+        setSession(response.data.user, response.data.session);
+      })
+      .catch(error => {
+        console.log('error to signin user', error.response);
+        // dispatch(authError(error.response.error.user));
+      })
   }
 };
 
-export const signOutUser = () => {
+export const signoutUser = () => {
+  return (dispatch, getState, { AuthAPI }) => {
+    return AuthAPI.signout().request
+      .then(response => {
+        console.log('signout user successfully', response);
+        dispatch(unauthUser())
+        resetSession();
+      })
+      .catch(error => {
+        console.log('error to signout user', error.response);
+        dispatch(authError(error.response.error.user));
+      })
+  }
+};
 
-}
