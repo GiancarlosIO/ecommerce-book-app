c4412231667314f455d851104f928f1cf6466553
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-04 01:33:11 -0500

    Add product list and product-card components

---
+++ app/javascript/app/components/products/product-card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductCard = (props) => (
+  <Link to={`products/${props.id}`} className="card">
+    <div className="card-img">
+      <img src={props.image} />
+    </div>
+    <div className="card-info">
+      <h3 className="card-info-title">{props.name.substr(0, 15)}...</h3>
+      <p className="product-card-description">{props.description.substr(0, 42)}...</p>
+      <div className="product-card-info">
+        <span className="product-card-price">Price: ${props.price}</span>
+        <span className="product-card-quantity">Quantity: {props.quantity}</span>
+      </div>
+    </div>
+  </Link>
+)
+
+export default ProductCard;
---
+++ app/javascript/app/components/products/product-list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getProducts } from '../../actions/product-actions';
+
+import ProductCard from './product-card';
+
+export class ProductList extends Component {
+
+  componentDidMount() {
+    console.log('Product list mounted');
+    this.props.dispatch(getProducts());
+  }
+
+  renderProducts = () => {
+    const { products } = this.props;
+    if (products) {
+      return Object.keys(products).map(key => {
+        return (
+          <ProductCard key={key} {...products[key]}/>
+        )
+      });
+    }
+  }
+
+  render() {
+    const { products } = this.props;
+    return (
+      <div>
+        <h1>Products</h1>
+        <div className="product-card-container">
+          { this.renderProducts() }
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({ products: state.products.all })
+export default connect(mapStateToProps)(ProductList);
