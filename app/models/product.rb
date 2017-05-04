d86a4777c2d69e48574e6369d15f2ff4102e6b97
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-03 22:35:37 -0500

    Add dependency for destroy products and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   has_many :product_category
-  has_many :categories, through: :product_category
+  has_many :categories, through: :product_category, dependent: :destroy
 end
