a686d8409114e195d844e51f7b954f7386f63161
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-04 01:36:36 -0500

    Add constants for products and categories

--- app/javascript/app/constants/index.js
+++ app/javascript/app/constants/index.js
@@ -1,4 +1,12 @@
+// auth constants
 export const AUTH_USER = 'AUTH_USER';
 export const UNAUTH_USER = 'UNAUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const SET_USER_DATA = 'SET_USER_DATA';
+// products constants
+export const SELECT_PRODUCT = 'SELECT_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+// categories constants
+export const SET_CATEGORIES = 'SET_CATEGORIES';
+export const SELECT_CATEGORY = 'SELECT_CATEGORY';
